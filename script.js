4eb851df8415816ddcc3e57278c425c551c1d770
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-17 00:35:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function mincost(arr)
-{ 
-//write your code here
+{ //sum of cost of each individual operation 
 // return the min cost
-  
+
+	arr.sort((a,b) => a-b);//sorting on basis of integerValues(byDefault string lekar karta tha)
+	let sum = arr[0];
+	let totalSum = 0;
+	for(let i=1; i<arr.length; i++){
+		sum += arr[i];
+		totalSum += sum;//add sums of all operations
+	}
+	return totalSum;
 }
 
 module.exports=mincost;
